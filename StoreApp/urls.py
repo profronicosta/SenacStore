211b2434ceb26ade2e0a53c86dd7238443e34c08
Roni Costa <profronicosta@gmail.com>
2023-11-29 22:28:17 -0300

    programando os templates

--- StoreApp/urls.py
+++ StoreApp/urls.py
@@ -4,5 +4,6 @@ from StoreApp import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('produtos/', views.produto_lista, name='produto_lista'),
-    path('produto/', views.produto_detalhe, name='produto_detalhe')
+    path('produtos/<int:id>', views.produto_lista_por_id, name='produto_lista_por_id'),
+    path('produto/<int:id>', views.produto_detalhe, name='produto_detalhe')
 ]
--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,12 +1,42 @@
 from django.shortcuts import render
-from StoreApp.models import Departamento
+from StoreApp.models import Departamento, Produto
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    produtos_em_destaque = Produto.objects.filter(destaque = True)
+
+    context = {
+        'produtos' : produtos_em_destaque
+    }
+
+    return render(request, 'index.html', context)
 
 def produto_lista(request):
-    return render(request, 'produtos.html')
+    # Buscando produtos no banco
+    produtos = Produto.objects.all()
 
-def produto_detalhe(request):
-    return render(request, 'produto_detalhes.html')
+    context = {
+        'produtos' : produtos,
+        'categoria' : 'Todos Produtos'
+    }
+    return render(request, 'produtos.html', context)
+
+def produto_lista_por_id(request, id):
+    # Buscando produtos no banco filtrando por depto
+    produtos = Produto.objects.filter(departamento_id = id)
+    #Buscando o depto no banco
+    departamento = Departamento.objects.get(id = id)
+
+    context = {
+        'produtos' : produtos,
+        'categoria' : departamento.nome
+    }
+    return render(request, 'produtos.html', context)
+
+def produto_detalhe(request, id):
+    produto = Produto.objects.get(id = id)
+
+    context = {
+        'produto' : produto
+    }
+    return render(request, 'produto_detalhes.html', context)
